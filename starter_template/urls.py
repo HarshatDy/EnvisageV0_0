dfacedb55b11cf8f9e0b177cb49f5c4e2d97c218
HarshatDy <dhanayat.harshat@gmail.com>
2024-11-15 07:31:06 +0530

    1)static input of HTML page in generate pages added2) HTML pages can be opened from the homepage itself

--- starter_template/urls.py
+++ starter_template/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('generate/', views.get_page, name='generate_html_page'),
-    path('<str:page_name>/', views.homepage, name='show_generated_page'),
+    path('<str:page_name>/', views.show_generated_page, name='show_generated_page'),
 ]
+
+
+def add_url(url_pattern, name, views_func):
+    urlpatterns.append(path(f'/{url_pattern}', views.views_func, name=name))
+    print(f" THIS Are the new URLS {url_pattern}")
--- starter_template/views.py
+++ starter_template/views.py
@@ -3,24 +3,32 @@ from django.http import HttpResponse
 import os
 import openai
 from . import openai_api as api, file_operations as fo 
+from . import urls 
 
 # For managing generated pages
 generated_pages = []
+for filename in os.listdir(os.path.join((os.path.dirname(os.path.abspath(__file__))), "templates")):
+    if filename.endswith(".html"):
+        generated_pages.append(filename)
+
+
+
 
 def homepage(request):
     return render(request, 'homepage.html', {'pages': generated_pages})
 
 
-
-
 def get_page(request):
 
     page_name = request.POST.get('page_name')
+    print(f" THe requested paage is {page_name}")
 
     # Get the page name from the request #### TODO : make this available from openai api
-    content="Create an HTML page with the title '{page_name}' and content 'Envisage - Creativity Meets Technology' in a sleek design."
+    content=f"Create an HTML page with the title {page_name} and content 'Envisage - Creativity Meets Technology' in a sleek design."
     content = api.openai_api_request(content)
     # print(content.data[0].content[0].text.value) ## print the content of the output form openai API.
+    
+    ################################ ---> Create a new URL and get the prints
     
     path = os.path.dirname(os.path.abspath(__file__))
     path = os.path.join(path, "templates")
@@ -35,3 +43,6 @@ def get_page(request):
 
     return redirect('homepage')
 
+
+def show_generated_page(request, page_name):
+    return render(request, f'{page_name}', {'page_name' :page_name})
